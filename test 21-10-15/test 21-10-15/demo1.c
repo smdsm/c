76e57bd121283ca42ca578053491044886866cdd
傅旭森 <3350256082@qq.com>
2021-10-15 23:21:11 +0800

    提交代码

---
+++ test 21-10-15/test 21-10-15/demo1.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int age = 100;
+
+
+int main()
+{
+	int age = 20;
+	printf("%d\n", age);
+	printf("hello world\n");
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+
+	return 0;
+}
