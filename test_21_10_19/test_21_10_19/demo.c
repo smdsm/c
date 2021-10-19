d1d222b3b66d0276d8ed9c22cdc1b234891e7d82
傅旭森 <3350256082@qq.com>
2021-10-19 23:07:07 +0800

    提交学习记录

---
+++ test_21_10_19/test_21_10_19/demo.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int MAX(int x,int y)
+{
+	int max = 0;
+	max = x > y ? x : y;
+	return max;
+}
+int main()
+{
+	int max = 0;
+	int num1 = 0;
+	int num2 = 0;
+	scanf("%d%d", &num1, &num2);
+	max = MAX(num1, num2);
+	printf("%d\n ", max);
+	return 0;
+}
